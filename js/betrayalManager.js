366392a06c64656b35d8bd42fc5bb713bbb7e388
neonwednesdays <nxiao@macrun.local>
2015-01-31 03:32:05 -0500

    JSON character searching now works appropriately

--- js/betrayalManager.js
+++ js/betrayalManager.js
@@ -1,37 +1,55 @@
 var numPlayers;//number of players max 6
 
-function player (name, character_name){
+function player (name, character){
 	this.name = name;
-	this.character_name = character_name;
-};
-
-var newPlayer = function(name){
-	var np = new player(name, 'test');
-	return np;
+	this.character = character;
 };
 
 $(document).ready(function(){
+	// Load JSON data
+	function loadJSON(player)
+    {
+       	var data_file = "http://www.elementten.com/BetrayalHelper/BetrayalCharacters.json";
+       	var http_request = new XMLHttpRequest();
+       	try{
+          	// Opera 8.0+, Firefox, Chrome, Safari
+          	http_request = new XMLHttpRequest();
+       	}catch (e){
+          	// Internet Explorer Browsers
+          	try{
+             	http_request = new ActiveXObject("Msxml2.XMLHTTP");
+          	}catch (e) {
+             	try{
+                	http_request = new ActiveXObject("Microsoft.XMLHTTP");
+             	}catch (e){
+                	// Something went wrong
+                	alert("Your browser broke!");
+                	return false;
+             	}	
+          	}
+       	}
+       	http_request.onreadystatechange  = function(){
+          	if (http_request.readyState == 4)
+          	{
+            // Javascript function JSON.parse to parse JSON data
+            var jsonObj = JSON.parse(http_request.responseText);
+            //oh man please work
+            console.log(player.character);
+            var jsonFound = jsonObj[player.character];
+
+           	$("#char_list").append(jsonFound[0].name);
+        	}
+       	}
+      	http_request.open("GET", data_file, true);
+       	http_request.send();
+    }
 	// Some semantic UI buisness
 	$('.ui.dropdown')
-      .dropdown({    
-      	transition: 'drop'
-		})
-    ;
-
-	// Place new Player with Character into the place
-	// $(".button.add_player").click(function(){
-
-	// 	var playerName = $("#player_name").text();
-	// 				console.log(playerName);
-
-	// 	if(playerName !== ''){
-	// 		$("#char_list").append(playerName + " playing " +$(this).text());
-	// 		resetForm("#player_name_input");
-	// 		$('.button.add_player').popup('show', false);
-	// 	}else{
-	// 		$('.button.add_player').popup('show', true);
-	// 	}
-	// });
+	  .dropdown({
+	    // you can use any ui transition
+	    transition: 'drop'
+	  })
+	;
 
 	$('.ui.form')
 	  .form({
@@ -59,15 +77,14 @@ $(document).ready(function(){
   		var $form = $('.ui.form');
   		var allfields = $form.form('get values');
 		if(allfields.name!='' && allfields.character!=''){
-			$("#char_list").append(allfields.name + " playing as " + allfields.character);
+			loadJSON(new player(allfields.name, allfields.character));
+			// $("#char_list").append(allfields.name + " playing as " + allfields.character);
 			$form.form('clear');
-
 		}
 	    e.preventDefault(); //usually use this, but below works best here.
 	});
-
-	
 });
+
 
 
 
