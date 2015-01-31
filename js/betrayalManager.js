e97eabb6bc5aef0dfc3b09161562ce47132fb719
neonwednesdays <nxiao@macrun.local>
2015-01-30 23:56:40 -0500

    Form submission now works

--- js/betrayalManager.js
+++ js/betrayalManager.js
@@ -19,28 +19,55 @@ $(document).ready(function(){
     ;
 
 	// Place new Player with Character into the place
-	$(".button.add_player").click(function(){
+	// $(".button.add_player").click(function(){
 
-		var playerName = $("#player_name_input").val();
-		if(playerName !== ''){
-			$("#char_list").append(playerName + " playing " +$(this).text());
-			resetForm("#player_name_input");
-			$('.button.add_player').popup('show', false);
-			console.log("yay");
-		}else{
-			$('.button.add_player').popup('show', true);
+	// 	var playerName = $("#player_name").text();
+	// 				console.log(playerName);
+
+	// 	if(playerName !== ''){
+	// 		$("#char_list").append(playerName + " playing " +$(this).text());
+	// 		resetForm("#player_name_input");
+	// 		$('.button.add_player').popup('show', false);
+	// 	}else{
+	// 		$('.button.add_player').popup('show', true);
+	// 	}
+	// });
+
+	$('.ui.form')
+	  .form({
+	    name: {
+	      identifier  : 'name',
+	      rules: [
+	        {
+	          type   : 'empty',
+	          prompt : 'Please enter your name'
+	        }
+	      ]
+	    },
+	    character: {
+	      identifier  : 'character',
+	      rules: [
+	        {
+	          type   : 'empty',
+	          prompt : 'Please select a character'
+	        }
+	      ]
+	    }
+	  });
+
+  	$('.ui.form').submit(function(e){ 
+  		var $form = $('.ui.form');
+  		var allfields = $form.form('get values');
+		if(allfields.name!='' && allfields.character!=''){
+			$("#char_list").append(allfields.name + " playing as " + allfields.character);
+			$form.form('clear');
 
 		}
+	    e.preventDefault(); //usually use this, but below works best here.
 	});
+
+	
 });
 
 
-// Resets the fields in a specified form ID
-function resetForm(formID){
-	$(formID)
-	  .not(':button, :submit, :reset, :hidden')
-	  .val('')
-	  .removeAttr('checked')
-	  .removeAttr('selected');
-}
 
