236f0da763a236bc6520b77e7d60311a4cd2153e
neonwednesdays <nxiao@macrun.local>
2015-01-30 19:57:59 -0500

    jquery adds players, bootstrap theme

--- scripts/betrayalManager.js
+++ scripts/betrayalManager.js
@@ -3,11 +3,29 @@ var numPlayers;//number of players max 6
 function player (name, character_name){
 	this.name = name;
 	this.character_name = character_name;
-}
+};
 
 var newPlayer = function(name){
 	var np = new player(name, 'test');
 	return np;
+};
+
+$(document).ready(function(){
+	
+});
+
+$("#red_char").click(function(){
+	var playerName = $("#player_name_input").val();
+	if(playerName !== ''){
+		$("#char_list").append(playerName + " playing " +$(this).text());
+	}
+	resetForm("#player_name_input");
+});
+
+function resetForm(formID){
+	$(formID)
+	  .not(':button, :submit, :reset, :hidden')
+	  .val('')
+	  .removeAttr('checked')
+	  .removeAttr('selected');
 }
-
-
