b7eab7e063d65cad9defa457fcb53a08337289f0
neonwednesdays <dxiao.ns@gmail.com>
2015-02-01 20:00:07 -0500

    refinagled the updateStats function
    there’s also characterStats.js which holds the default stats, not using
    json anymore

--- js/characterStats.js
+++ js/characterStats.js
@@ -4,16 +4,16 @@ var numPlayers = 0;
 function character(name){
 	this.name = name;
 }
-function stat(name, defaultIndex, spread){
+function statObject(name, defaultIndex, spread){
 	this.name = name;
 	this.defaultIndex = defaultIndex;
-	this.currentIndex = defaultIndex;
+	this._index = defaultIndex;
 	this.spread = spread;
 }
 //holds character data
 var oxbellows = new character("Ox Bellows");
 	oxbellows.shortname = 'oxbellows';
-	oxbellows.speed = new stat('speed', 4, [2, 2, 2, 3, 4, 5, 5, 6])
+	oxbellows.speed = new statObject('speed', 4, [2, 2, 2, 3, 4, 5, 5, 6])
 	// oxbellows.stats = [this.speed, this.might, this.sanity, this.knowledge];
 //does stuff in a functino to create a player
 function createPlayer(data){
@@ -31,10 +31,12 @@ function createPlayer(data){
 	}).appendTo('.'+characterClass);
 	//give that baby a title
 	$('.content.' + characterClass).append('<a class="header">'+ p.character.name+ '</a>');
-	makeStatCard('.content.' + characterClass, characterClass, p.character.speed);
+	makeStatCard(p, p.character.speed);
 }
 
-function makeStatCard(htmltarget, name, stat){
+function makeStatCard(player, stat){
+	var name = player.character.shortname;
+	var htmltarget = '.content.' + name;
 	//make a tiny card
 	$('<div></div>',{
 		class:'ui tiny card '+name
@@ -43,19 +45,23 @@ function makeStatCard(htmltarget, name, stat){
 	$('.ui.tiny.card.'+name).append('<div class="header">'+stat.name+'</div>');
 	//stat holder
 	$('<div></div>',{
-		class:'ui horizontal list speed stat holder '+name
+		class:'ui horizontal list stat holder '+stat.name+' '+name
 	}).appendTo('.ui.tiny.card.'+name);
 	//buttons holder
 	$('<div></div>',{
 		class:'2 fluid ui basic buttons '+name
 	}).appendTo('.ui.tiny.card.'+name);
 	//append the stats
+	$('.stat.holder.'+name).append('<div class="inactive item stat '+stat.name+' '+name+'" id="speed '+name+' '+'empty'+'">'+'</div>')
 	$.map(stat.spread, function(val, i){
-		$('.stat.holder.'+name).append('<div class="inactive item speed stat '+name+'" id="speed '+i+'">'+val+'</div>')
-	})
+		$('.stat.holder.'+name).append('<div class="inactive item stat '+stat.name+' '+name+'" id="speed '+name+' '+i+'">'+val+'</div>')
+	})		
+	$('.stat.holder.'+name).append('<div class="inactive item stat '+stat.name+' '+name+'" id="speed '+name+' '+'empty'+'">'+'</div>')
 	//buttons
-	$('.2.fluid.ui.basic.buttons.'+name).append('<div class="ui button '+name+'" id="minus_stat">-</div>'+'<div class="ui button '+name+'" id="plus_stat">+</div>')
-}
+	$('.2.fluid.ui.basic.buttons.'+name).append('<div class="ui button '+name+'" id="minus '+stat.name+' '+name+'">-</div>'+'<div class="ui button '+name+'" id="plus speed '+name+'">+</div>')
+	makeStatClickListener(player, player.character.speed, 1);
+	makeStatClickListener(player, player.character.speed, -1);
+}	
 
 
 
--- js/statManager.js
+++ js/statManager.js
@@ -1,54 +1,70 @@
 var maxIndex = 8;
+var defaultStatX = -7;
+var easeDist = 42;
+
+//takes in the player, which stat is being manipulated, and then the direction to move it
+function makeStatClickListener(player, stat, dir){
+	var characterClass = player.character.shortname;
+	var buttonid = '';
+	updateStat(stat, characterClass, 0);
+	//var targetid = '';
+	switch(dir){
+		case 1://plus
+			buttonid = "div[id='"+'plus '+stat.name+' '+characterClass+"']";
+			$(buttonid).on('click', function(){
+				updateStat(stat, characterClass, 1);
+			});
+			break;
+		case -1://minus
+			buttonid = "div[id='"+'minus '+stat.name+' '+characterClass+"']";
+			$(buttonid).on('click', function(){
+				updateStat(stat, characterClass, -1);
+			});
+			break;
+		default:
+			console.log('oops, what happened when making this listener...')
+			break;
+	}
+}
+//take in a specific stat holder and animate it
+function updateStat(stat, charclassname, dir){
+	if((stat._index>0 && dir<0)||(stat._index<maxIndex-1 && dir>0)||dir==0){
+		stat._index = stat._index + dir;
+		var targetid = '.stat.holder'+'.'+stat.name+'.'+charclassname;
+		$(targetid).animate({
+			right:''+((stat._index*easeDist)-defaultStatX+'px'),
+			easing:'easeOutQuart'},350, function(){	
+		});
+		makeInActive(stat, charclassname, stat._index-dir);//previous stat
+		makeActive(stat, charclassname, stat._index);
+	}
+}
+//toggle the inactive stat for css
+function makeInActive(stat, charclassname, idnum){
+	var targetid = ("div[id='"+stat.name+" "+charclassname+" "+idnum+"']");
+	$(targetid).addClass("inactive");
+	$(targetid).removeClass("active");
+}
+//toggle the active class for css
+function makeActive(stat, charclassname, idnum){
+	var targetid = ("div[id='"+stat.name+" "+charclassname+" "+idnum+"']");
+	$(targetid).addClass("active");
+	$(targetid).removeClass("inactive");
+}
 $(document).ready(function(){
 
 	//this is just testing for one stat :(
-	var index = 0;
-	var defaultStatX = -7;
-	var easeDist = 42;
-	makeActive("speed",index);
-	updateStat();
+	var fake = new statObject('fake', 0, [1,2,3,4,5,6,7,8]);
+	updateStat(fake,'test',0);
+
+	//TEST STUFF
 	// Decrement a stat
-	$('#minus_stat').click(function(e){
-		if(index>0){
-			makeInActive("speed",index);
-			index-=1;
-			makeActive("speed",index);
-			updateStat();
-		}
+	$('#minus_stat_test').click(function(e){
+		updateStat(fake,'test',-1);
 	});
 	// Increment a stat 
-	$('#plus_stat').click(function(e){
-		if(index<maxIndex-1){
-			makeInActive("speed",index);
-			index+=1;
-			makeActive("speed",index);
-
-			updateStat();
-		}
+	$('#plus_stat_test').click(function(e){
+		updateStat(fake,'test',1);
 	});
-	//take in a specific stat holder and animate it
-	function updateStat(){
-		if(index>2){
-			// easeDist = 50;
-		}else{
-			// easeDist = 45;
-		}
-
-		$('.speed.stat.holder').animate({
-			right:''+((index*easeDist)-defaultStatX+'px'),
-			easing:'easeOutQuart'},350, function(){
-				
-		});
-		
-	}
-	function makeInActive(type, idnum){
-		var targetid = ("div[id='"+type+" "+idnum+"']");
-		$(targetid).addClass("inactive");
-		$(targetid).removeClass("active");
-	}
-	function makeActive(type, idnum){
-		var targetid = ("div[id='"+type+" "+idnum+"']");
-		$(targetid).addClass("active");
-		$(targetid).removeClass("inactive");
-	}
+	
 });
